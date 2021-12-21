9470a85e95384f315e22a41a2200f042310966fc
photoncrusher <82525783+photoncrusher@users.noreply.github.com>
2021-12-21 11:19:52 +0700

    Update knn_temp.py

--- knn_temp.py
+++ knn_temp.py
@@ -11,6 +11,28 @@ class KNearestNeighbor(object):
     self.X_train = X
     self.y_train = y
     
+  def compute_distances_two_loops(self, X):
+    # cham nhat
+    num_test = X.shape[0]
+    num_train = self.X_train.shape[0]
+    dists = np.zeros((num_test, num_train))
+    for i in range(num_test):
+      for j in range(num_train):
+       
+        dists[i][j] = np.sqrt(np.sum((X[i] - self.X_train[j])**2))
+     
+    return dists
+
+  def compute_distances_one_loop(self, X):
+    # cham nhi
+    num_test = X.shape[0]
+    num_train = self.X_train.shape[0]
+    dists = np.zeros((num_test, num_train))
+    for i in range(num_test):
+  
+      dists[i,:] = np.sqrt( np.sum( ( X[i] - self.X_train ) ** 2, axis=1) )
+  
+    return dists
   
   def compute_distances_no_loops(self, X):
 
